76942462a5e0b347f40cfce711f4d4d4825eb68d
CarolineMorton <carolinemorton@gmx.com>
2020-01-20 18:49:07 +0000

    add sys and os bits for the readme

---
+++ notebooks/diffable_python/importing_code.py
@@ -0,0 +1,35 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     notebook_metadata_filter: all,-language_info
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+import pandas as pd
+
+# Path to your local code via sys and os
+
+import os
+import sys
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+# Then import either the functions (better) 
+
+from code.custom_functions import multiplier
+
+# or import the whole file of functions
+
+from code import custom_functions
+
+
--- notebooks/diffable_python/notebook_importing_data.py
+++ notebooks/diffable_python/notebook_importing_data.py
@@ -21,6 +21,11 @@
 import pandas as pd
 import os
 
+import sys
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
 from code.custom_functions import calbmi
 
 # ### Import data and process it 
